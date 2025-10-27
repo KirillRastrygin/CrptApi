096158489ae9cefa02709e2c69f63c44ee3fb8ab
Kirill <kirill.rastrygin@gmail.com>
2025-10-27 23:05:20 +0300

    Generate Getters and Setters for data classes

--- src/main/java/org/example/CrptApi.java
+++ src/main/java/org/example/CrptApi.java
@@ -127,83 +127,6 @@ public class CrptApi {
         }
     }
 
-    // ==================== Внутренние классы ====================
-
-    /**
-     * Класс документа
-     */
-    public static class Document {
-        public Description description;
-        public String doc_id;
-        public String doc_status;
-        public String doc_type;
-        public boolean importRequest;
-        public String owner_inn;
-        public String participant_inn;
-        public String producer_inn;
-        public String production_date;
-        public String production_type;
-        public List<Product> products;
-        public String reg_date;
-        public String reg_number;
-
-        public Document(Description description, String doc_id, String doc_status, String doc_type, boolean importRequest, String owner_inn, String participant_inn, String producer_inn, String production_date, String production_type, List<Product> products, String reg_date, String reg_number) {
-            this.description = description;
-            this.doc_id = doc_id;
-            this.doc_status = doc_status;
-            this.doc_type = doc_type;
-            this.importRequest = importRequest;
-            this.owner_inn = owner_inn;
-            this.participant_inn = participant_inn;
-            this.producer_inn = producer_inn;
-            this.production_date = production_date;
-            this.production_type = production_type;
-            this.products = products;
-            this.reg_date = reg_date;
-            this.reg_number = reg_number;
-        }
-    }
-
-    /**
-     * Класс описания к документу
-     */
-    public static class Description {
-        public String participantInn;
-
-        public Description(String participantInn) {
-            this.participantInn = participantInn;
-        }
-
-    }
-
-    /**
-     * Описание товара
-     */
-    public static class Product {
-        public String certificate_document;
-        public String certificate_document_date;
-        public String certificate_document_number;
-        public String owner_inn;
-        public String producer_inn;
-        public String production_date;
-        public String tnved_code;
-        public String uit_code;
-        public String uitu_code;
-
-        public Product(String certificate_document, String certificate_document_date, String certificate_document_number, String owner_inn, String producer_inn, String production_date, String tnved_code, String uit_code, String uitu_code) {
-            this.certificate_document = certificate_document;
-            this.certificate_document_date = certificate_document_date;
-            this.certificate_document_number = certificate_document_number;
-            this.owner_inn = owner_inn;
-            this.producer_inn = producer_inn;
-            this.production_date = production_date;
-            this.tnved_code = tnved_code;
-            this.uit_code = uit_code;
-            this.uitu_code = uitu_code;
-        }
-    }
-
-
     public static void main(String[] args) throws InterruptedException {
 
         final String CREATE_DOCUMENT_API_URL = "https://ismp.crpt.ru/api/v3/lk/documents/create";
@@ -262,6 +185,268 @@ public class CrptApi {
         executor.shutdown();
         executor.awaitTermination(1, TimeUnit.MINUTES);
         System.out.println("All threads finished.");
+    }
+
+    // ==================== Внутренние классы ====================
+
+    /**
+     * Класс документа
+     */
+    public static class Document {
+
+        public Description description;
+        public String doc_id;
+        public String doc_status;
+        public String doc_type;
+        public boolean importRequest;
+        public String owner_inn;
+        public String participant_inn;
+        public String producer_inn;
+        public String production_date;
+        public String production_type;
+        public List<Product> products;
+        public String reg_date;
+        public String reg_number;
+
+        public Document(Description description, String doc_id, String doc_status, String doc_type, boolean importRequest, String owner_inn, String participant_inn, String producer_inn, String production_date, String production_type, List<Product> products, String reg_date, String reg_number) {
+            this.description = description;
+            this.doc_id = doc_id;
+            this.doc_status = doc_status;
+            this.doc_type = doc_type;
+            this.importRequest = importRequest;
+            this.owner_inn = owner_inn;
+            this.participant_inn = participant_inn;
+            this.producer_inn = producer_inn;
+            this.production_date = production_date;
+            this.production_type = production_type;
+            this.products = products;
+            this.reg_date = reg_date;
+            this.reg_number = reg_number;
+        }
+
+        public Description getDescription() {
+            return description;
+        }
+
+        public void setDescription(Description description) {
+            this.description = description;
+        }
+
+        public String getDoc_id() {
+            return doc_id;
+        }
+
+        public void setDoc_id(String doc_id) {
+            this.doc_id = doc_id;
+        }
+
+        public String getDoc_status() {
+            return doc_status;
+        }
+
+        public void setDoc_status(String doc_status) {
+            this.doc_status = doc_status;
+        }
+
+        public String getDoc_type() {
+            return doc_type;
+        }
+
+        public void setDoc_type(String doc_type) {
+            this.doc_type = doc_type;
+        }
+
+        public boolean isImportRequest() {
+            return importRequest;
+        }
+
+        public void setImportRequest(boolean importRequest) {
+            this.importRequest = importRequest;
+        }
+
+        public String getOwner_inn() {
+            return owner_inn;
+        }
+
+        public void setOwner_inn(String owner_inn) {
+            this.owner_inn = owner_inn;
+        }
+
+        public String getParticipant_inn() {
+            return participant_inn;
+        }
+
+        public void setParticipant_inn(String participant_inn) {
+            this.participant_inn = participant_inn;
+        }
+
+        public String getProducer_inn() {
+            return producer_inn;
+        }
+
+        public void setProducer_inn(String producer_inn) {
+            this.producer_inn = producer_inn;
+        }
+
+        public String getProduction_date() {
+            return production_date;
+        }
+
+        public void setProduction_date(String production_date) {
+            this.production_date = production_date;
+        }
+
+        public String getProduction_type() {
+            return production_type;
+        }
+
+        public void setProduction_type(String production_type) {
+            this.production_type = production_type;
+        }
+
+        public List<Product> getProducts() {
+            return products;
+        }
+
+        public void setProducts(List<Product> products) {
+            this.products = products;
+        }
+
+        public String getReg_date() {
+            return reg_date;
+        }
+
+        public void setReg_date(String reg_date) {
+            this.reg_date = reg_date;
+        }
+
+        public String getReg_number() {
+            return reg_number;
+        }
+
+        public void setReg_number(String reg_number) {
+            this.reg_number = reg_number;
+        }
+    }
+
+    /**
+     * Класс описания к документу
+     */
+    public static class Description {
+
+        public String participantInn;
+
+        public Description(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+        public String getParticipantInn() {
+            return participantInn;
+        }
+
+        public void setParticipantInn(String participantInn) {
+            this.participantInn = participantInn;
+        }
+
+    }
+
+    /**
+     * Описание товара
+     */
+    public static class Product {
+        public String certificate_document;
+        public String certificate_document_date;
+        public String certificate_document_number;
+        public String owner_inn;
+        public String producer_inn;
+        public String production_date;
+        public String tnved_code;
+        public String uit_code;
+        public String uitu_code;
+
+        public Product(String certificate_document, String certificate_document_date, String certificate_document_number, String owner_inn, String producer_inn, String production_date, String tnved_code, String uit_code, String uitu_code) {
+            this.certificate_document = certificate_document;
+            this.certificate_document_date = certificate_document_date;
+            this.certificate_document_number = certificate_document_number;
+            this.owner_inn = owner_inn;
+            this.producer_inn = producer_inn;
+            this.production_date = production_date;
+            this.tnved_code = tnved_code;
+            this.uit_code = uit_code;
+            this.uitu_code = uitu_code;
+        }
+
+        public String getCertificate_document() {
+            return certificate_document;
+        }
+
+        public void setCertificate_document(String certificate_document) {
+            this.certificate_document = certificate_document;
+        }
+
+        public String getCertificate_document_date() {
+            return certificate_document_date;
+        }
+
+        public void setCertificate_document_date(String certificate_document_date) {
+            this.certificate_document_date = certificate_document_date;
+        }
+
+        public String getCertificate_document_number() {
+            return certificate_document_number;
+        }
+
+        public void setCertificate_document_number(String certificate_document_number) {
+            this.certificate_document_number = certificate_document_number;
+        }
+
+        public String getOwner_inn() {
+            return owner_inn;
+        }
+
+        public void setOwner_inn(String owner_inn) {
+            this.owner_inn = owner_inn;
+        }
+
+        public String getProducer_inn() {
+            return producer_inn;
+        }
+
+        public void setProducer_inn(String producer_inn) {
+            this.producer_inn = producer_inn;
+        }
+
+        public String getProduction_date() {
+            return production_date;
+        }
+
+        public void setProduction_date(String production_date) {
+            this.production_date = production_date;
+        }
+
+        public String getTnved_code() {
+            return tnved_code;
+        }
+
+        public void setTnved_code(String tnved_code) {
+            this.tnved_code = tnved_code;
+        }
+
+        public String getUit_code() {
+            return uit_code;
+        }
+
+        public void setUit_code(String uit_code) {
+            this.uit_code = uit_code;
+        }
+
+        public String getUitu_code() {
+            return uitu_code;
+        }
+
+        public void setUitu_code(String uitu_code) {
+            this.uitu_code = uitu_code;
+        }
     }
 
 }
